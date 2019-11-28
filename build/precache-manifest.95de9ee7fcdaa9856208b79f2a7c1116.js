d718051dc2398a458362b173570fdb730278ef12
sserichenko <sserichenko@gmail.com>
2019-11-28 13:37:10 +0200

    first commit hw3_2 react

---
+++ build/precache-manifest.95de9ee7fcdaa9856208b79f2a7c1116.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b919a26ed8c8d32974bb6e50a50f196f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c553d0dd920d8f2d8e7b",
+    "url": "./static/css/main.523137d2.chunk.css"
+  },
+  {
+    "revision": "af87baa33454bfa966c8",
+    "url": "./static/js/2.f6fab082.chunk.js"
+  },
+  {
+    "revision": "c553d0dd920d8f2d8e7b",
+    "url": "./static/js/main.d4bf7518.chunk.js"
+  },
+  {
+    "revision": "f19e712a31fa1b997752",
+    "url": "./static/js/runtime-main.277ee49b.js"
+  }
+]);
